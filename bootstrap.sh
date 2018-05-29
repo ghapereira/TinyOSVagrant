99cb144e5ed53c37ac39fae8f8edb5e85cedc64e
Gustavo Pereira <ghapereira>
2018-05-29 09:20:43 -0300

    Change default theme to kardan

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,3 +25,9 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
 chsh -s /usr/bin/zsh ubuntu
 chsh -s /usr/bin/zsh vagrant
+
+# Change oh-my-zsh default theme to kardan
+sed -i -e 's/robbyrussell/kardan/g' ~/.zshrc
+
+# cd into /vagrant
+echo "cd /vagrant" >> ~/.zshrc
