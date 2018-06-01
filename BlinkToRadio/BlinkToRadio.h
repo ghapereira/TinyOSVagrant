94f5f63a8cff6d7c6198452ecfac2acb0fca4183
Gustavo Pereira <ghapereira>
2018-06-01 19:50:03 -0300

    Struct contendo formato das mensagens adicionada

--- BlinkToRadio/BlinkToRadio.h
+++ BlinkToRadio/BlinkToRadio.h
@@ -3,6 +3,24 @@ typedef nx_struct BlinkToRadioMsg {
     nx_uint16_t counter;
 } BlinkToRadioMsg;
 
+typedef nx_struct iot_tp2_grupo7_payload {
+    nx_uint16_t TEMPERATURA;
+    nx_uint16_t LUMINOSIDADE;
+    nx_uint8_t  ID_RESTANTE;
+    nx_uint8_t  PAYLOAD[13];
+} iot_tp2_grupo7_payload;
+
+typedef nx_struct iot_tp2_struct {
+    nx_uint16_t SRC_ADDR;
+    nx_uint16_t DST_ADDR;
+    nx_uint8_t  TYPE;
+    nx_uint8_t  LENGTH;
+    nx_uint8_t  HOPS;
+    nx_uint16_t FATHER_ID;
+
+    iot_tp2_grupo7_payload PAYLOAD;
+} iot_tp2_struct;
+
 enum {
     AM_BLINKTORADIOMSG = 6,
     TIMER_PERIOD_MILLI = 250
